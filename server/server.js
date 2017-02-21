dfd5cc840c19d31ebcf3fa8ec8a2d55e89d2c503
hashmat00 <info@hashmat00.com>
2017-02-21 22:13:29 +0000

    initial udpate

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const path = require("path");
 const express = require("express");
 const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(publicPath));
 
-
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(port, () => {
     console.log('server has started');
 });
